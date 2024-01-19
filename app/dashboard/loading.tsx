42520b4e6ccce5b5347f43a16fbf2aabf2f1f745
Ferreya Ricardo <ferreyra.gluti@gmail.com>
2024-01-19 01:31:15 -0300

    Skeleton/StreamingData

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import DashboardSkeleton from '../ui/skeletons';
+
+const loading = () => {
+  return <DashboardSkeleton />;
+};
+
+export default loading;
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -19,7 +19,7 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -34,6 +34,7 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    await new Promise((resolve) => setTimeout(resolve, 1500));
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -57,6 +58,7 @@ export async function fetchCardData() {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
+    await new Promise((resolve) => setTimeout(resolve, 1500));
     const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
     const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
     const invoiceStatusPromise = sql`SELECT
